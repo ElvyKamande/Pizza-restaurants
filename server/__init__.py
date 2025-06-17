dc33a909baf425e6413e4a3e4e685cb389051031
ElvyKamande <elvy.waithera@student.moringaschool.com>
2025-06-17 18:15:24 +0300

    working on controllers

---
+++ server/__init__.py
@@ -0,0 +1,1 @@
+# Makes `models` a package. No content needed unless you want to import all models here.
---
+++ server/models/restaurant_pizza.py
@@ -0,0 +1,15 @@
+from ..app import db
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+
+    def validate(self):
+        errors = []
+        if self.price < 1 or self.price > 30:
+            errors.append("Price must be between 1 and 30")
+        return errors
